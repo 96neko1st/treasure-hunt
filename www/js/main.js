08d4cf581f39ff56a4036cddbc2d615b41d2dad7
96neko1st <seed.kira07@gmail.com>
2017-12-23 14:23:21 +0900

    add:機能追加 宝を押すと、ランダムな画像にする。選ばれなかった箱は薄くする。

--- www/js/main.js
+++ www/js/main.js
@@ -1,2 +1,29 @@
-// This is a JavaScript file
-
+(function(){
+    "use strict";
+    
+    var boxes = document.getElementsByClassName("box");
+    
+    var contents = [
+        "coin.png",
+        "empty.png",
+        "cobra.png"
+    ]
+    
+    function initBox(){
+        for(var i = 0; i < boxes.length; i++){
+            boxes[i].addEventListener("click", function(){
+                if(this.className.indexOf("shake") === -1){
+                    return;
+                }
+                for(var j = 0; j < boxes.length; j++){
+                    boxes[j].src = "img/" + contents[Math.floor(Math.random() * contents.length)];
+                    boxes[j].className = "box disabled"
+                }
+                this.className = "box";
+                document.getElementById("btn").className = "";
+            });
+        }
+    }
+    
+    initBox();
+})();
